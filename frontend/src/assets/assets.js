d464afd98802e8c66f83fdff581635e8f2b1e4b9
haile-paa <haileyesuseyasu09@gmail.com>
2025-02-09 01:31:40 +0300

    portfolio

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,11 @@
 import logo from "./pa logo for white.png";
 import picture from "./DSC_0141.jpg";
 import jersey from "./jersey-shop.png";
+import crypto from "./cryptoPlace.png";
 
 export const assets = {
   logo,
   picture,
   jersey,
+  crypto,
 };
